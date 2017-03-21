4a9f6079939cc936b44d4e4b98712ad8523ede7a
cochransd <cochransd@g.cofc.edu>
2017-03-20 22:05:33 -0700

    Initial commit

---
+++ app/src/main/java/steve_cochran/stockqoutes_cochran/MainActivity.java
@@ -0,0 +1,13 @@
+package steve_cochran.stockqoutes_cochran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
